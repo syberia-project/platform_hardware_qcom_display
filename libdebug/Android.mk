973fe8ff07a7e191ce811f9080c721d6935c4268
Pig <pig.priv@gmail.com>
2020-09-19 09:19:29 +0800

    display: Remove COPY_HEADERS usage
    Change-Id: I964a11f036e03fb9fa2dec05a1698c118b06b421
    Signed-off-by: DennySPb <dennyspb@gmail.com>

--- libdebug/Android.mk
+++ libdebug/Android.mk
@@ -8,5 +8,6 @@ LOCAL_SHARED_LIBRARIES        := libdl
 LOCAL_CFLAGS                  := -DLOG_TAG=\"SDM\" -Wall -std=c++11 -Werror -fno-operator-names
 LOCAL_CLANG                   := true
 LOCAL_SRC_FILES               := debug_handler.cpp
+LOCAL_EXPORT_C_INCLUDE_DIRS := $(LOCAL_PATH)
 
 include $(BUILD_SHARED_LIBRARY)
